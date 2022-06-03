0f1b46fc70cf60dcdf3c9e0d9398908a31cf0725
moutendy <christophermoutendy@gmail.com>
2022-06-03 06:43:24 +0200

    m

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientComponent } from './client/client.component';
+import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 import { ServerComponent } from './server/server.component';
 
 const routes: Routes = [
@@ -9,6 +10,9 @@ const routes: Routes = [
   },
   {
     path: 'server',component:ServerComponent
+  },
+  {
+    path: 'form',component:ProfileEditorComponent
   }
 ];
 
